941b63d4cd7366d5d63e9192824faca99a6a1514
Elizabeth Geno <205633872+LizGeno@users.noreply.github.com>
2025-10-24 17:38:48 -0300

    GET products

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// REQUERIMIENTO 1
 
+
+//console.log(process.argv);
+// const metodo =process.argv[2];
+// const recurso = process.argv[3];
+
+let [, , metodo, recurso]= process.argv;
+metodo = metodo.toUpperCase();
+recurso = recurso.toLowerCase();
+
+if(metodo == 'GET' && recurso == "products"){
+const response = await fetch('https://fakestoreapi.com/products')
+const data = await response.json();
+console.log(data);
+}
